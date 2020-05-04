984ad6a7d81d0190f3420ddef406de5ee7cc8591
Udi Oron <udioron@gmail.com>
2020-05-04 12:51:05 +0300

    added a very simple expense model

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    title = models.CharField(max_length=300)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    created_at = models.DateField()
