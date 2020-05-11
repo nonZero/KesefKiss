978bb34aaf0efe9fd04e4ad1c01435f47be83e87
Udi Oron <udioron@gmail.com>
2020-05-11 12:41:16 +0300

    Added validators

--- expenses/models.py
+++ expenses/models.py
@@ -1,9 +1,13 @@
+from django.core import validators
 from django.db import models
 
 
 class Expense(models.Model):
     title = models.CharField(max_length=300)
-    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    amount = models.DecimalField(decimal_places=2, max_digits=10, validators=[
+        validators.MaxValueValidator(1_000_000),
+        validators.MinValueValidator(0.50),
+    ])
     created_at = models.DateField()
 
     def __str__(self):
--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from expenses.models import Expense
@@ -13,3 +14,19 @@ class ExpensesTestCase(TestCase):
         o.full_clean()
         o.save()
         self.assertTrue(o.is_expensive())
+
+    def test_expense_too_big(self):
+        o = Expense(
+            title="Boeing 747",
+            amount=212_000_000,
+            created_at=f"2020-02-23",
+        )
+        self.assertRaises(ValidationError, o.full_clean)
+
+    def test_expense_too_small(self):
+        o = Expense(
+            title="Bazooka",
+            amount=0.02,
+            created_at=f"2020-02-23",
+        )
+        self.assertRaises(ValidationError, o.full_clean)
