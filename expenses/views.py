5276f7d2fd356898cc60747d4e4dc1716a888b73
Udi Oron <udioron@gmail.com>
2020-05-11 13:13:21 +0300

    Simple template example

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def expense_list(request):
+    return render(request,
+                  "expenses/expense_list.html", {
+                      'word': 'xyzzy',
+                      'nums': [23, 4, 5, 323, 2, 55],
+                      'mylist': [
+                          'milk',
+                          'cookies',
+                          'coffee',
+                      ],
+                  })
