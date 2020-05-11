75a8ad833e22b6c6cf6e15eb32b81cdd245974b9
Udi Oron <udioron@gmail.com>
2020-05-11 13:19:13 +0300

    added expenses list

--- expenses/views.py
+++ expenses/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 
+from expenses.models import Expense
+
 
 def expense_list(request):
-    return render(request,
-                  "expenses/expense_list.html", {
-                      'word': 'xyzzy',
-                      'nums': [23, 4, 5, 323, 2, 55],
-                      'mylist': [
-                          'milk',
-                          'cookies',
-                          'coffee',
-                      ],
-                  })
+    return render(request, "expenses/expense_list.html", {
+        'object_list': Expense.objects.all(),
+    })
