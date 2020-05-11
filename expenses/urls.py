092fd2895fb6bead567b0dac88aeb63b2611caf4
Udi Oron <udioron@gmail.com>
2020-05-11 13:34:24 +0300

    relax urls

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "expenses"
 
 urlpatterns = [
-    path('kazabobo/', views.expense_list, name="list"),
-    path('kukuriku67x/<int:id>/', views.expense_detail, name="detail"),
+    path('', views.expense_list, name="list"),
+    path('<int:id>/', views.expense_detail, name="detail"),
 ]
