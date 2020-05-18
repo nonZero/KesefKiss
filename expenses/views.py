5175fdd759d11441aaf550c3676b85ba5aa882d0
Udi Oron <udioron@gmail.com>
2020-05-18 13:28:18 +0300

    cookie demo

--- expenses/views.py
+++ expenses/views.py
@@ -4,10 +4,14 @@ from expenses.models import Expense
 
 
 def expense_list(request):
-    return render(request, "expenses/expense_list.html", {
+    print(request.COOKIES)
+    resp = render(request, "expenses/expense_list.html", {
         'object_list': Expense.objects.all(),
         'foo': '<b>shalom!</b> <i>bye!</i>',
     })
+    # resp['X-FOO'] = "yoyoyoyyoyoy!"
+    resp.set_cookie('mycookie', "myvalue")
+    return resp
 
 
 def expense_detail(request, id: int):
