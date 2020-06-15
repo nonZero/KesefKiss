0f59dfb1558e999cd31a2f138072238170ac38f9
Udi Oron <udioron@gmail.com>
2020-06-15 19:40:50 +0300

    Disable debug on heroku

--- kkiss/settings.py
+++ kkiss/settings.py
@@ -1,9 +1,9 @@
-import django_heroku
 import os
+
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -11,11 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'rom1yzi)$!9fepa#kwpxw=8!10gou_73uxr36nvh85!yhin-6('
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+IS_HEROKU = bool(os.environ.get("DYNO"))
+DEBUG = not IS_HEROKU
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -68,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kkiss.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -78,7 +76,6 @@ DATABASES = {
         'NAME': 'kkiss',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -115,10 +111,18 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+LOGGING['loggers'][''] = {
+    'handlers': ['console'],
+    'level': 'INFO',
+}
+LOGGING['loggers']['django'] = {
+    'handlers': ['console'],
+    'level': 'INFO',
+}
